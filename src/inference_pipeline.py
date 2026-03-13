0283440d967357f7d6bd91a3c353c63ac81c60cd
SaraGC-Bcn <sgarciacas@hotmail.com>
2026-03-13 13:08:27 +0100

    update text in labels

--- src/inference_pipeline.py
+++ src/inference_pipeline.py
@@ -27,7 +27,7 @@ def predict_diabetes(X: pd.DataFrame) -> tuple[str, float]:
     prediction = model.predict(X_transformada)[0]
     probability = model.predict_proba(X_transformada)[0][1]
 
-    label = 'Diabetes' if prediction == 1 else 'No Diabetes'
+    label = 'es posible que TENGA DIABETES' if prediction == 1 else 'es posible que NO TENGA DIABETES'
 
     return label, probability
 
